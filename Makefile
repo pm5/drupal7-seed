0e3080369513b106b51b4a88062c4d9d82f15f1f
Pomin Wu <pomin5@gmail.com>
2014-12-12 19:00:18 +0800

    fix user docker group

--- Makefile
+++ Makefile
@@ -3,18 +3,18 @@ PWD=`pwd`
 .PHONY: build test run start stop clean
 
 build:
-	docker build --rm=true -t pomin5/docker-drupal7 .
+	docker build -t pomin5/drupal7 .
 
-test:
-	docker run -it --rm=true -p 8080:80 --name drupal7 -v $(PWD):/app pomin5/docker-drupal7 /bin/bash
+test-docker:
+	docker run -it --rm -p 8080:80 --name drupal7 -v $(PWD):/app pomin5/drupal7 /bin/bash
 
-run:
-	docker run -d -p 8080:80 --name=drupal7 -v $(PWD):/app pomin5/docker-drupal7
+run-docker:
+	docker run -d -p 8080:80 --name=drupal7 -v $(PWD):/app pomin5/drupal7
 
-start:
+start-docker:
 	docker start drupal7
 
-stop:
+stop-docker:
 	docker stop drupal7
 
 clean:
--- bootstrap.sh
+++ bootstrap.sh
@@ -6,27 +6,14 @@ DB_NAME=app
 DB_USER=root
 DB_PASS=foobar
 
-apt-get update
-apt-get install -y php5 php5-gd php5-curl php5-sqlite php-pear unzip php5-mysql php5-pgsql apache2 libapache2-mod-php5 curl
+usermod -aG docker vagrant
 
-pear channel-discover pear.drush.org
-pear install drush/drush
+apt-get update && \
+  apt-get upgrade -y
+
+curl -s https://get.docker.io/ | sh
 
 mkdir -p ${PUBLIC_DIR}
-
-cd ${APP_DIR}
-drush make bootstrap.make ${PUBLIC_DIR}
-cd ${PUBLIC_DIR}
-drush site-install -y standard --site-name='Drupal7 Sandbox' --account-name=admin --account-pass=admin --db-url=sqlite://sites/default/files/.ht.sqlite
-
-rm -rf /var/www
-ln -fs ${PUBLIC_DIR} /var/www
-sed -i 's/AllowOverride None/AllowOverride all/g' /etc/apache2/sites-enabled/000-default
-sudo a2enmod rewrite
-sudo a2enmod expires
-sudo a2enmod auth_basic
-sudo a2enmod headers
-sudo service apache2 restart
 
 echo "mysql-server mysql-server/root_password password $DB_PASS" | debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password $DB_PASS" | debconf-set-selections
@@ -40,3 +27,6 @@ cat > /home/vagrant/.my.cnf <<END
 [client]
 password=$DB_PASS
 END
+
+cd ${APP_DIR}
+make run-docker
