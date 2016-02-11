04c42edf5829d97ef42e845fc1fd37aaf6e558fe
Pomin Wu <pomin5@gmail.com>
2016-02-11 16:20:37 +0800

    Move provision directory to playbooks

--- scripts/ansible-install.sh
+++ scripts/ansible-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd provision
+cd playbooks
 ansible-galaxy install -r ansible-galaxy.yml
 
-git clone https://github.com/pm5/ansible-role-drupal.git roles/pm5.drupal
-git clone https://github.com/pm5/ansible-role-glusterfs.git roles/pm5.glusterfs
+git clone https://github.com/pm5/ansible-role-drupal.git roles/pm5.drupal || true
+git clone https://github.com/pm5/ansible-role-glusterfs.git roles/pm5.glusterfs || true
